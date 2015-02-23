3c7c6e4390d5ed63e2c1662463f38233dc96a81e
Michal Rus <m@michalrus.com>
2015-02-23 23:00:57 +0100

    Create NutritionalRatios chart

--- src/main/scala/com/michalrus/nofatty/chart/Chart.scala
+++ src/main/scala/com/michalrus/nofatty/chart/Chart.scala
@@ -1,11 +1,27 @@
 package com.michalrus.nofatty.chart
 
 import org.jfree.chart.JFreeChart
+import org.jfree.data.time.Day
+import org.joda.time.LocalDate
+
+object Chart {
+  val LastDays = 100
+  val Grams = "Mass [g]"
+  val Percent = "%"
+  val Protein = "Protein"
+  val Fat = "Fat"
+  val Carbohydrate = "Carbohydrate"
+  val Fiber = "Fiber"
+}
 
 trait Chart {
 
   val title: String
   val chart: JFreeChart
   def refresh(): Unit
+
+  import language.implicitConversions
+  implicit def localDateToDay(d: LocalDate): Day =
+    new Day(d.getDayOfMonth, d.getMonthOfYear, d.getYear)
 
 }
--- src/main/scala/com/michalrus/nofatty/chart/NutritionalRatios.scala
+++ src/main/scala/com/michalrus/nofatty/chart/NutritionalRatios.scala
@@ -1,22 +1,55 @@
 package com.michalrus.nofatty.chart
 
+import java.text.SimpleDateFormat
+
+import com.michalrus.nofatty.data.{ Days, EatenProduct }
+import org.jfree.chart.axis.DateAxis
+import org.jfree.chart.renderer.xy.{ StackedXYBarRenderer, StandardXYBarPainter }
 import org.jfree.chart.{ ChartFactory, JFreeChart }
-import org.jfree.data.xy.{ XYSeries, XYSeriesCollection }
+import org.jfree.data.time.TimeTableXYDataset
+import org.joda.time.LocalDate
 
 object NutritionalRatios extends Chart {
+  import com.michalrus.nofatty.chart.Chart._
 
   override val title: String = "Nutritional ratios"
 
+  private[this] val dataset = new TimeTableXYDataset()
+
   override val chart: JFreeChart = {
-    val dataset = new XYSeriesCollection()
-    val series = new XYSeries("fat")
-    series.add(0.0, 0.0)
-    series.add(1.0, 1.0)
-    series.add(2.0, 0.5)
-    dataset.addSeries(series)
-    ChartFactory.createXYLineChart(title, "day", "", dataset)
+    val c = ChartFactory.createXYBarChart("", "", true, "Stacked mass-to-protein ratios", dataset)
+    c.getXYPlot.setDomainAxis({
+      val a = new DateAxis
+      a.setDateFormatOverride(new SimpleDateFormat("d-MMM"))
+      a
+    })
+    c.getXYPlot.setRenderer({
+      val r = new StackedXYBarRenderer(0.05)
+      r.setBarPainter(new StandardXYBarPainter)
+      r.setShadowVisible(false)
+      r
+    })
+
+    c
   }
 
-  override def refresh(): Unit = ???
+  override def refresh(): Unit = {
+    val today = LocalDate.now
+    val days = (0 until Chart.LastDays).toVector.reverse map today.minusDays map (d ⇒ (d, Days.find(d))) dropWhile (_._2.isEmpty)
+
+    days foreach {
+      case (date, Some(day)) if day.eatenProducts.nonEmpty ⇒
+        val nv = EatenProduct.sum(day.eatenProducts)
+        val d = nv.protein
+        dataset.add(date, nv.protein / d, Protein)
+        dataset.add(date, nv.fat / d, Fat)
+        dataset.add(date, nv.carbohydrate / d, Carbohydrate)
+        dataset.add(date, nv.fiber / d, Fiber)
+      case (date, _) ⇒
+        Seq(Protein, Fat, Carbohydrate, Fiber) foreach (n ⇒ dataset.add(date, 0.0, n))
+    }
+  }
+
+  refresh()
 
 }
