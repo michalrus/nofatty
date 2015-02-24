f683dd0d012e779931269f780d1f744fb032d313
Michal Rus <m@michalrus.com>
2015-02-24 18:10:33 +0100

    Import weights

--- src/main/scala/com/michalrus/nofatty/data/DBDummy.scala
+++ src/main/scala/com/michalrus/nofatty/data/DBDummy.scala
@@ -1,12 +1,10 @@
 package com.michalrus.nofatty.data
 
 import java.io.File
-import java.util.UUID
 
 import com.github.tototoshi.csv.CSVReader
-import scala.slick.driver.SQLiteDriver.simple._
-import com.michalrus.nofatty.data.DB.{ discard ⇒ d }
-import org.joda.time.{ DateTimeZone, DateTime, LocalDate, LocalTime }
+import com.michalrus.nofatty.data.DB.{discard => d}
+import org.joda.time.{DateTime, DateTimeZone, LocalDate}
 
 object DBDummy {
 
@@ -17,35 +15,16 @@ object DBDummy {
     r
   }
 
-  val prod = csv("/home/m/prod.csv") drop 1 map {
-    case List(id, name, kcal, prot, fat, carb, fib) ⇒
-      (id.toInt, name, NutritionalValue(kcal.toDouble, prot.toDouble, fat.toDouble, carb.toDouble, fib.toDouble))
+  val weights = csv("/home/m/weight.csv") map {
+    case List(t, w) ⇒
+      (new LocalDate(t.toLong, DateTimeZone.getDefault), (w.toDouble * 10.0).round.toDouble / 10.0)
   }
 
-  val prodUUIDs = prod.map { case (id, _, _) ⇒ id → UUID.randomUUID }.toMap
-
-  val eaten = csv("/home/m/eaten.csv") drop 1 map {
-    case List(date, time, id, grams) ⇒
-      (LocalDate.parse(date), LocalTime.parse(time), id.toInt, grams.toDouble)
-  }
-
-  DB.db withTransaction { implicit session ⇒
-    d {
-      DB.basicProducts ++= (prod map {
-        case (id, name, nv) ⇒
-          (prodUUIDs(id), DateTime.now, name, nv.kcal.toString, nv.kcal, nv.protein.toString,
-            nv.protein, nv.fat.toString, nv.fat, nv.carbohydrate.toString, nv.carbohydrate, nv.fiber.toString, nv.fiber)
-      })
-    }
-
-    d { DB.days ++= (eaten.map(_._1).distinct map ((_, DateTime.now, DateTimeZone.getDefault, "", None))) }
-
-    d {
-      DB.eatenProducts ++= (eaten map {
-        case (date, time, id, grams) ⇒
-          (date, time, prodUUIDs(id), grams.toString, grams)
-      })
-    }
+  weights foreach {
+    case (date, weight) ⇒
+      val day = Days.find(date) getOrElse Day(date, DateTime.now, DateTimeZone.getDefault, "", None, Seq.empty)
+      val mod = day.copy(lastModified = DateTime.now, weightExpr = weight.toString, weight = Some(weight))
+      Days.commit(mod)
   }
 
 }
